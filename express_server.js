0f49e79402dc9539eb721c3561d538512aac1ed7
Paige Kato <paige@getflow.com>
2019-01-22 22:13:57 -0800

    Deploy to heroku

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const bcrypt = require('bcrypt');
